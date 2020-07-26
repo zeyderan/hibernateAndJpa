1d9ee588c6eea4f47a804e14a44fbcbef82e637f
zeyderan <aykutbayraktar34@gmail.com>
2020-07-26 13:12:58 +0300

    inital commit

---
+++ src/test/java/com/javacourse/project/hiberateAndJpa/HiberateAndJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javacourse.project.hiberateAndJpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiberateAndJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
